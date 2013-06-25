e3d9af477ac4d76d2d55d21cbb1a7ba084b841da
phhui <phhui@qq.com>
2013-06-25 17:45:05 +0800

    简易文章采集发布工具
    暂时只针对指定网站进行单贴采集
    采集完后发到指定网站
    暂时不支持图片下载

---
+++ readMail/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace readMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_usn = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.webView = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txt_usn
+            // 
+            this.txt_usn.Location = new System.Drawing.Point(5, 4);
+            this.txt_usn.Name = "txt_usn";
+            this.txt_usn.Size = new System.Drawing.Size(660, 21);
+            this.txt_usn.TabIndex = 1;
+            this.txt_usn.Text = "http://www.isapt.com/article/51902.html";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(671, 2);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 5;
+            this.btn_submit.Text = "登  录";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(3, 31);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_output.Size = new System.Drawing.Size(1376, 327);
+            this.txt_output.TabIndex = 6;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(752, 2);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 8;
+            this.btn_send.Text = "发  表";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // webView
+            // 
+            this.webView.Location = new System.Drawing.Point(5, 364);
+            this.webView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(1374, 398);
+            this.webView.TabIndex = 9;
+            this.webView.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webView_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 762);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.txt_usn);
+            this.Name = "Form1";
+            this.Text = "邮件阅读器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_usn;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.WebBrowser webView;
+    }
+}
+
---
+++ readMail/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace readMail
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string,string> htmlList=new Dictionary<string,string>();
+        private string title;
+        private string content;
+        private int type = 0;//0：登录 1：进入发帖子页面 2：帖子发布 3：帖子发成功
+        public Form1()
+        {
+            InitializeComponent();
+            htmlList.Add("<strong>", "phhui_加粗头");
+            htmlList.Add("</strong>", "phhui_加粗尾");
+            htmlList.Add("<br />", "phhui_换行");
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+
+            Byte[] pageData = wc.DownloadData(txt_usn.Text); //从指定网站下载数据
+
+            string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
+
+            //string pageHtml = Encoding.UTF8.GetString(pageData); //如果获取网站页面采用的是UTF-8，则使用这句
+            title = pageHtml.Substring(pageHtml.IndexOf("<h1>") + 4, pageHtml.IndexOf("</h1>") - pageHtml.IndexOf("<h1>") - 4) + "\r\n\r\n";
+            content = UnSafeHTMLFilter(pageHtml.Substring(pageHtml.IndexOf("u950309\";") + 9, pageHtml.IndexOf("<!--赞一下-->") - pageHtml.IndexOf("u950309\";") - 9));
+            txt_output.Text = "标题：" + title;
+            txt_output.Text += "内容：" + content;
+        }
+        public String UnSafeHTMLFilter(string html)
+        {
+            //html=saveHtml(html);
+            Regex regex1 = new Regex(@"<[^>]+>", RegexOptions.IgnoreCase);// 定义HTML标签的正则表达式
+            Regex regex2 = new Regex(@"<[/s]*?script[^>]*?>[/s/S]*?<[/s]*?//[/s]*?script[/s]*?>", RegexOptions.IgnoreCase);// 定义script的正则表达式{或<script[^>]*?>[/s/S]*?<//script>
+            html = regex1.Replace(html, ""); //过滤HTML标记 
+            html = regex2.Replace(html, ""); //过滤script属性 
+            return resetHtml(html);
+        }
+        public String saveHtml(string html)
+        {
+            foreach (KeyValuePair<string, string> l in htmlList)
+            {
+                html = html.Replace(l.Key,l.Value);
+            }
+            return html;
+        }
+        public String resetHtml(string html)
+        {
+            foreach (KeyValuePair<string, string> l in htmlList)
+            {
+                html = html.Replace(l.Value, l.Key);
+            }
+            return html;
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            webView.Navigate("56lea.com");
+        }
+
+        private void webView_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (type == 0)
+            {
+                if (webView.Document.GetElementById("username") == null)
+                {
+                    webView.Navigate("56lea.com");
+                    return;
+                }
+                webView.Document.GetElementById("username").SetAttribute("value", "oO小菜Oo");
+                webView.Document.GetElementById("password").SetAttribute("value", "password");
+                for (int j = 0; j < webView.Document.All.Count; j++)
+                {
+                    if (webView.Document.All[j].GetAttribute("value").IndexOf("登录") > -1)
+                    {
+                        webView.Document.All[j].InvokeMember("click");
+                    }
+                }
+                type = 1;
+            }
+            else if (type == 1)
+            {
+                webView.Navigate("http://56lea.com/forum.php?mod=post&action=newthread&fid=49");
+                type = 2;
+            }
+            else if (type == 2)
+            {
+                webView.Document.GetElementById("subject").SetAttribute("value", title);
+                webView.Document.GetElementById("e_textarea").SetAttribute("value", content);
+                webView.Document.GetElementById("postsubmit").InvokeMember("click");
+            }
+            else
+            {
+                type = 0;
+            }
+        }
+    }
+}
